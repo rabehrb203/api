f25148f062cbfa174d384bdcad545acc47717ca5
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-04-04 19:02:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const compression = require("compression");
 
 const app = express();
 const port = 3000;
-const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
+app.use(compression());
 
 // API endpoint for getting a list of manga titles and links
 app.get("/manga/titles", async (req, res) => {
-  const base_url = "https://lekmanga.net/manga/page/";
-  const max_page = 2; // تحديد عدد الصفحات
+  const base_url = "https://like-manga.net/manga/page/";
+  const max_page = 63; // تحديد عدد الصفحات
+
   let manga_titles = [];
   let promises = [];
 
   try {
     for (let page = 1; page <= max_page; page++) {
       const url = base_url + page + "/";
-      // تضمين رأس "User-Agent" في خيارات الطلب
-      const options = {
-        headers: {
-          "User-Agent": userAgent
-        }
-      };
-      promises.push(axios.get(url, options));
+      promises.push(axios.get(url));
     }
 
     const responses = await Promise.all(promises);
 
     responses.forEach((response) => {
       const $ = cheerio.load(response.data);
-      const items = $(".col-12.col-md-6.badge-pos-1");
+      const items = $(".col-6.col-md-2.badge-pos-1");
 
       items.each((index, element) => {
-        const manga_title = $(element)
-          .find("div.post-title.font-title")
-          .text()
-          .trim();
+        const manga_title = $(element).find("h3.h5").text().trim();
         const manga_cover = $(element).find("img").attr("src");
 
         const manga_title_link = $(element)
           .find("a")
           .attr("href")
-          .substring(27);
+          .substring(28);
 
         manga_titles.push({
           title: manga_title,
@@ -56,43 +49,49 @@ app.get("/manga/titles", async (req, res) => {
     res.status(500).json({ error: `An error occurred: ${error}` });
   }
 });
-
 // API endpoint for getting details of a specific manga
 app.get("/manga/details/:manga_link", async (req, res) => {
   const manga_link = req.params.manga_link;
 
   try {
     const response = await axios.get(
-      `https://lekmanga.net/manga/${manga_link}`
+      "https://like-manga.net/manga/" + manga_link
     );
     const $ = cheerio.load(response.data);
-    const other_info_sections = $(
-      ".post-content_item, .post-status .post-content_item"
-    );
+    const other_info_section = $(".post-content .post-content_item");
+    const other_info_section2 = $(".post-status .post-content_item");
 
     let other_info = {};
 
-    other_info_sections.each((index, element) => {
+    other_info_section.each((index, element) => {
       const heading = $(element)
         .find("h5")
         .text()
         .trim()
-        .replace("التقييم", "rating")
-        .replace("المؤلف", "author")
-        .replace("الرسام", "artist")
-        .replace("التصنيف", "categories")
+        .replace("تقييم", "rating")
+        .replace("التصنيفات", "categories")
         .replace("المرتبة", "rank")
         .replace("النوع", "type")
-        .replace("اسماء اخرى", "otherNames")
-        .replace("سنة الانتاج", "release")
+        .replace("أسماء أخرى", "otherNames");
+      const content = $(element).find(".summary-content").text().trim();
+      other_info[heading] = content;
+    });
+    other_info_section2.each((index, element) => {
+      const heading = $(element)
+        .find("h5")
+        .text()
+        .trim()
+        .replace("الإصدار", "release")
         .replace("الحالة", "status");
       const content = $(element).find(".summary-content").text().trim();
       other_info[heading] = content;
     });
 
+    // Convert the set back to an object
+
     res.json(other_info);
   } catch (error) {
-    res.status(500).json({ error: `An error occurred: ${error.message}` });
+    res.status(500).json({ error: `An error occurred: ${error}` });
   }
 });
 
@@ -102,7 +101,7 @@ app.get("/manga/chapters/:manga_link", async (req, res) => {
 
   try {
     const response = await axios.get(
-      `https://lekmanga.net/manga/${manga_link}`
+      "https://like-manga.net/manga/" + manga_link
     );
     const $ = cheerio.load(response.data);
     const chapters = $(".page-content-listing .wp-manga-chapter");
@@ -121,17 +120,16 @@ app.get("/manga/chapters/:manga_link", async (req, res) => {
 
     res.json(manga_chapters);
   } catch (error) {
-    res.status(500).json({ error: `An error occurred: ${error.message}` });
+    res.status(500).json({ error: `An error occurred: ${error}` });
   }
 });
-
 // API endpoint for getting images of a specific chapter
 app.get("/manga/images/:manga_title/:chapter_number", async (req, res) => {
   const manga_title = req.params.manga_title;
   const chapter_number = req.params.chapter_number;
 
   try {
-    const chapter_link = `https://lekmanga.net/manga/${manga_title}/${chapter_number}/`;
+    const chapter_link = `https://like-manga.net/manga/${manga_title}/${chapter_number}/`;
     const response = await axios.get(chapter_link);
     const $ = cheerio.load(response.data);
     const images = $(".reading-content .wp-manga-chapter-img");
@@ -144,7 +142,7 @@ app.get("/manga/images/:manga_title/:chapter_number", async (req, res) => {
 
     res.json(chapter_images);
   } catch (error) {
-    res.status(500).json({ error: `An error occurred: ${error.message}` });
+    res.status(500).json({ error: `An error occurred: ${error}` });
   }
 });
 
