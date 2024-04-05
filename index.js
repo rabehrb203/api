f3c7e9e7912a1d9571a260b6dafa0f6209a635d7
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-04-05 17:43:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,24 +133,7 @@ app.get("/manga/images/:manga_title/:chapter_number", async (req, res) => {
 
    const chapter_link = `https://lekmanga.net/manga/${manga_title}/${chapter_number}/`;
    const headers = {
-  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  'accept-language': 'ar-DZ,ar;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5',
-  'cookie': 'wpmanga-reading-history=W3siaWQiOjMzMDMsImMiOiI0OTE2NjEiLCJwIjoxLCJpIjoiIiwidCI6MTcxMjI1MTE3MH0seyJpZCI6NTQyLCJjIjoiMjAzMzciLCJwIjoxLCJpIjoiIiwidCI6MTcxMjI2MDM2OH1d; cf_clearance=VRNUDP.HTNmpgMK.E5d53_oJFqytn.saGHXZSeahLIQ-1712328028-1.0.1.1-9IedFmnayA3n6h0ApS7yL1XJEnTB0OXm_.sPIXWWrdCHqB8I1MnMTsIMvmHHLxrXL4yNr44QfkYsuSXN7IlgKw',
-  'dnt': '1',
-  'if-modified-since': 'Fri, 22 Mar 2024 07:10:34 GMT',
-  'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-  'sec-ch-ua-arch': '"x86"',
-  'sec-ch-ua-bitness': '"64"',
-  'sec-ch-ua-full-version': '"123.0.6312.106"',
-  'sec-ch-ua-full-version-list': '"Google Chrome";v="123.0.6312.106", "Not:A-Brand";v="8.0.0.0", "Chromium";v="123.0.6312.106"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-model': '""',
-  'sec-ch-ua-platform': '"Windows"',
-  'sec-ch-ua-platform-version': '"15.0.0"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'navigate',
-  'sec-fetch-site': 'same-origin',
-  'upgrade-insecure-requests': '1',
+
   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 };
 
